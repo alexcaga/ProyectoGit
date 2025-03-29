cdabc984534ca76606d74c87f09f22284ed7d76f
AlexFPCadiz <alex-guillermo.carpio.garcia.alu@iesfernandoaguilar.es>
2025-03-29 13:23:14 +0100

    Cambios en Hola.java, eliminación del archivo Adios.txt y creación de Saludo.txt

--- Hola.java
+++ Hola.java
@@ -1,5 +1,5 @@
 public class Hola {
-    public static main(String[] args) {
-	System.out.println("Hola a todo el mundo!");
+    public static void main(String[] args) {
+	    System.out.println("Hola Mundo!");
     }
 }
